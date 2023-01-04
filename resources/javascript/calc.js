5499b02581420fb2b8f3cd2b7f333012f34084f1
Eurykles <119136075+Eurykles@users.noreply.github.com>
2023-01-04 11:17:47 -0500

    added an operator function

--- resources/javascript/calc.js
+++ resources/javascript/calc.js
@@ -33,3 +33,18 @@ let divide = function(array) {
     };
     return solution;
 };
+
+let operate = function(num1, operator, num2) {
+    switch(operator) {
+        case '-':
+            return subtract([num1, num2]);
+        case '+':
+            return add([num1, num2]);
+        case '*':
+            return multiply([num1, num2]);
+        case '/':
+            return divide([num1, num2]);
+    };
+};
+
+console.log(operate(5, '/', 7));
